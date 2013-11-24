7f0fb56ea1557f1e265751a168809c7bfc91f30f
wesleybits <wesley.bitomski@gmail.com>
2013-11-23 21:55:30 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ronin
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ronin "0.1.0-SNAPSHOT"
+  :description "Idiomatic wrapper for Kyoto Cabinet for Clojure"
+  :url "https://github.com/wesleybits/ronin"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-marginalia "0.7.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.fallabs/kyotocabinet-java "1.24"]])
