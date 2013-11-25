1a204e853df442ac31b4e1f6adc7ccd336b0b15f
wesleybits <wesley.bitomski@gmail.com>
2013-11-24 21:12:10 -0500

    more work on Ronin; coder-friendlier DB creation and connection; seperate name-space for specification maps; lovely docs

--- src/ronin/core.clj
+++ src/ronin/core.clj
@@ -1,247 +1,166 @@
+;; ##Ronin
+;; ###Or: Ignoble Mercinaries With Scary Military Training
+;; ####Or: Eastern Knights Errant
+;; #####Or: Sane-er Access To A Kyoto Cabinet
+
+;; Kyoto Cabinet is a pretty boss thing.  It's fast, light-weight,
+;; and concurrency friendly.  It's primary data representation is
+;; straight-up binary, so we can do some pretty wild things, like
+;; key music files by arbetrary images.
+
+;; However, it can be pretty insane to access via it's Java 
+;; bindings.  Check out 
+;; [it's DB docs](http://fallabs.com/kyotocabinet/javadoc/kyotocabinet/DB.html),
+;; and scroll on over to `.open(String, int)`.  The `.open` 
+;; specification alone is a short novella that gets into extreme 
+;; detail over how to specify a fully tuned connection.  Not that 
+;; you would need to get into such extreme detail when doing many 
+;; light-weight things, and it's nice that it's there, but paths 
+;; like `"-#log=/opt/kc/errors.log#logkind=error"`
+;; can be daunting for new-commers.
+
+;; But this is Clojure, so we can do _much_ better!
+
 (ns ronin.core
+  (:use ronin.specs)
   (:require [clojure.edn :as edn])
   (:import [kyotocabinet DB Visitor FileProcessor]
            [java.util List ArrayList Map]))
 
-(defn- byte? 
-  "Tests for a proper byte."
-  [elt]
-  (try 
-    (= elt (byte elt))
-    (catch Exception e 
-      false)))
-
 (defn- byte-array?
   "Tests for an array of bytes."
   [elt]
-  (and (= (type elt)
-          (type (to-array [])))
-       (reduce #(and %1 (byte? %2))
-               (vec elt))))
+  (= (type elt)
+     (type (.getBytes "ronin"))))
 
-(def ^{:doc "A lookup table for Kyoto Cabinet's DB:open options.
+(defn make-db
+  "#Create a new DB object.
 
- * `:synch`: auto synchronization, saves changes to the DB file.
- * `:transaction`: auto transaction, rewinds changes on error.
- * `:create`: create a casket (DB file), if it doesn't exist.
- * `:no-lock`: non-locking reads and writes, use with extreme care!
- * `:no-repair`: don't fix the DB if something goes wrong, use with care!
- * `:read`: read mode
- * `:write`: write mode
- * `:try-lock`: locking reads and writes. Safe but slow.
- * `:truncate`: open with data truncation. Improves speed, but screws with your data."}
-  open-options
-  {:sync        DB/OAUTOSYNC
-   :transaction DB/OAUTOTRANS
-   :create      DB/OCREATE
-   :no-lock     DB/ONOLOCK
-   :no-repair   DB/ONOREPAIR
-   :read        DB/OREADER
-   :truncate    DB/OTRUNCATE
-   :try-lock    DB/OTRYLOCK
-   :write       DB/OWRITER})
+It takes only one optional parameter, `throws?`, which defaults to true.
+When false, Kyoto Cabinet will not throw errors, so you'll have to check
+the `DB.error()` method to see what went wrong."
+  ([] (make-db true))
+  ([throws?]
+     (if throws? 
+       (DB. DB/GEXCEPTIONAL)
+       (DB.))))
 
-(def ^{:doc "A lookup table for merge options.
+(defn open-db!
+  "#Open a DB object.
 
- * `:add`: add mode. Keep the existing value.
- * `:append`: append mode. Append the new value indiscriminately.
- * `:replace`: replace mode. Modify the old value.
- * `:overwrite`: overwrite mode. Replace the old value indiscriminately."}
-  merge-options
-  {:add       DB/MADD
-   :append    DB/MAPPEND
-   :replace   DB/MREPLACE
-   :overwrite DB/MSET})
+Takes a DB object and a map with the following elements:
 
-(def ^{:dynamic true
-       :doc "##The Database Object.  
+ * `:type`: Required, must be one of the keys in `ronin.specs.db-specs`.
+ * `:filename`: Sometimes required.  If you're using an in-memory DB,
+   then this parameter is ignored.
+ * `:tuning`: Optional map of string parameters and their values, as
+   specified in [Kyoto Cabinet's DB docs](http://fallabs.com/kyotocabinet/javadoc/kyotocabinet/DB.html).  This option isn't cooked, much unlike all the other ones.
 
-Dealt with using the `with-db` special form."}
-  db nil)
+.. and a vector of access modes, which should be keys in `ronin.specs.open-options`.
 
-(defn accept-bulk
-  "## Accept Bulk
+Returns the opened DB object.
 
-Sends a visitor to an array of keys.
+An example would be opening a DB on a file hash, in create and write mode, and performs
+error logging to a file and compresses the resulting hash file when done:
 
- * `keys`: a vector of keys to visit.  Works best if they're strings.
- * `visitor`: the visitor used for this operation.
- * `write?`: optional arg, default is false.  Set to true if the visitor will be
-   writing to the database.
+    (open-db! my-db
+      {:type :file-hash
+       :filename \"./yojimbo\"
+       :tuning {\"log\"      \"./yojimbo.log\",
+                \"logkinds\" \"error\",
+                \"opts\"     \"lc\"}}
+      [:create :write])
 
-Returns a boolean: true if successful, false if it failed
+When finished, be sure to `.close` it."
+  [db specs modes]
+  (let [needs-filename #{:file-hash
+                         :file-tree
+                         :directory-hash
+                         :directory-tree
+                         :plain-text}
 
-Note: Operations are performed atomically, so other threads trying to access the
-listed records will be blocked.  To avoid deadlock, any explicit operations must
-not be performed here."
-  ([keys visitor] 
-     (accept-bulk keys visitor false))
-  ([keys visitor write?]
-     (.accept_bulk db keys write?)))
+        db-type (do (assert (keyword? (:type specs))
+                            ":type needs to be a keyword")
+                    (assert (not (nil? (get db-specs (:type specs))))
+                            ":type needs to be found in db-specs")
+                    (:type specs))
 
-(defn accept
-  "#Accept
+        db-file (if (some needs-filename db-type)
+                  (do (assert (string? (:filename specs))
+                              ":filename needs to be a string")
+                      (assert (not= "" (:filename specs))
+                              ":filename cannot be an empty string")
+                      (str (:filename specs) (get db-specs db-type)))
+                  (get db-specs db-type))
 
-Sends a visitor to a single key.
+        tuned-db-file (if (map? (:tuning specs))
+                        (-> (:tuning specs)
+                            (map (fn [[k v]] (str k "=" v)))
+                            (reduce #(str %1 "#" %2))
+                            (str db-file "#"))
+                        db-file)
+        
+        db-modes (do (assert (vector? modes)
+                             ":modes must be a vector")
+                     (assert (-> (map #(get open-options %) modes)
+                                 (map number?)
+                                 (reduce #(and %1 %2)))
+                             "every specified mode must be in `open-options`")
+                     (-> (map #(get open-options %) modes)
+                         (apply bit-or 0)))]
 
- * `key`: a key to visit.  Works best if it's a string.
- * `visitor`: the visitor. 'Nuf said.
- * `write?`: optional arg, default is false.  Set to true if the visitor will be
-writing to this key.
-
-Returns a boolean: true if successful, false if it failed
-
-Note: Operations are performed atomically, so other threads trying to access the
-listed records will be blocked.  To avoid deadlock, any explicit operations must
-not be performed here."
-  ([key ^Visitor visitor] 
-     (accept key visitor false))
-  ([key ^Visitor visitor write?]
-     (.accept key visitor write?)))
-
-(defn compare-and-swap
-  "##Compare and swap!
-
-For synchronization!  Compare and swap takes a key, an old value and
-a new value.  If the given old value matches what's in the DB, then
-it's replaced by the new value, otherwise nothing happens.
-
-Returns true if the swap happens, false otherwise."
-  [key old new]
-  (.cas db key old new))
-
-(defn check
-  "##Check
-
-Given a key, it checks to see if it's in the database.
-
-Returns the size of the entry if it's there, nil otherwise."
-  (let [key-bytes (if (byte-array? key) key (.getBytes key))
-        size? (.check db key-bytes)]
-    (if (< size? 0) nil size)))
-
-(defn clear
-  "#Hoses the database!!!"
-  []
-  (.clear db))
-
-(defn copy
-  "##Copy
-
-Copies the database to a destination file."
-  [dest]
-  (.copy db dest))
-
-(defn dump-snapshot
-  "##Dump Snapshot
-
-Dumps the current running state of the database into a destination file."
-  [dest]
-  (.dump_snapshot db dest))
-
-(defn load-snapshot
-  "##Load Snapshot
-
-Loads a previously taken snapshot into the current DB object."
-  [snapshot]
-  (.load_snapshot db snapshot))
-
-(defn count
-  "Gives the number of keys in the database."
-  []
-  (let [size? (.count db)]
-    (if (< size? 0) nil size?)))
-
-(defn match-prefix
-  "Finds records whose keys begin with the given prefix. Optional
-argument, `max-elts` specifies at most how many to retrieve; defaults
-to no limit."
-  ([prefix] (match-prefix prefix -1))
-  ([prefix max-elts]
-       (seq (.match_prefix db prefix max-elts))))
-
-(defn match-regex
-  "Finds records whose keys matches the given regex string.  The
-regex must be a string, not a Clojure regex, otherwise it just 
-won't work.  Optional argument, `max-elts` specifies at most how 
-many to retrieve; defaults to no limit."
-  ([rx] (match-regex rx -1))
-  ([rx max-els]
-     (seq (.match_regex rx max-elts))))
-
-(defn match-similar
-  "Finds records whose keys are kinda like the sample given.  This
-uses the levenshtien distence, so other args are needed.
-
- * `origin`: the like-string we're trying to match
- * `range`: the 'distance' from our origin we're willing to match.
- * `utf-8?`: Optional, default is true.  Set to false if you don't want
-   to treat keys like utf-8 strings.  I don't understand why you wouldn't
-   want to...
- * `max-elts`: Optional, default is no limit.  Set to the number of result
-   you want to at-most have."
-
-  ([origin range] (match-similar origin range true -1))
-  ([origin range opt]
-     (if (integer? opt)
-       (match-similar origin range true opt)
-       (match-similar origin range opt -1)))
-  ([origin range utf-8? max-elts]
-     (seq (.match_similar db origin range utf-8? max-elts))))
-
-(defn visit-everything
-  "##Visitor Iteration
-
-This sends a visitor along all the records atomically.  I don't need to
-warn you that this __HUGE__ operation is deadlock Grand Central, so don't
-use it if you are doing something rather involved, or have other processes
-in your database."
-  ([visitor] (visit-everything visitor false))
-  ([visitor write?] 
-     (.iterate db visitor write?)))
-
-(defn transaction
-  "##Transactions!
-
-Transactions group reads and writes together in a way that allows you to
-back out of any manipulations that could prove to be bad for your data.
-
- * `hard?`: an optional boolean.  Do we synchronize with the hardware 
-   memory, or logically with the filesystem?  Defaults to false.
- * `op`: a thunk that represents your operation.  It must return non-nil and
-   non-false if you wish to commit your actions."
-  ([op] (transaction false op))
-  ([hard? op]
-     (.begin_transaction db hard?)
-     (let [res (op)]
-       (if (or (nil? res) (not res))
-         (.end_transaction db false)
-         (.end_transaction db true)))))
+    (.open db tuned-db-file db-modes)
+    db))
 
 (defmacro with-db
-  "## With DB Macro
+  "#Very Modestly Managed DB access.
 
-This beautific sucker will handle opening up a Kyoto Cabinet database object
-using the provided parameters and open-specs, bind `db` to the created 
-database, and execute any forms given to it.  After all is said and done, 
-it closes the database and releases all it's resources completely.
+This is a convienence macro that simply creates a DB object, opens it
+with the provided spec map, executes the provided body and closes it 
+afterward.  It does nothing else.  The opened DB object is bound to
+the given symbol, so you can use it like this:
 
-Within this form, `db` is bound the desired casket with the specified open
-options, so convienent access to the object itself is still possible, in case 
-you need to do something that I haven't thought of."
-  [[casket & open-opts] & body]
-  `(let [merged-options (->> (map #(get open-options % 0) ~open-opts)
-                             (apply bit-or 0))]
-     (binding [db (DB. DB/GEXCEPTIONAL)]
-       (.open ~casket merged-options)
-       (do ~@body)
-       (.synchronize db false nil)
-       (.close db))))
+    (with-db [my-db {:type :cache-hash 
+                     :modes [:create :read :write]
+                     :throws? true}]
+      (add-edn! my-db \"entry-1\" {:some \"data\"})
+      (get-edn my-db \"entry-1\"))
 
-;; ----
-;; The following are specially abastracted forms that make using Kyoto Cabinet
+Here, we do extend the open specification to include:
+ 
+ * `:throws?`: should we throw errors or not?  Defaults to true if 
+   not found.
+ * `:modes`: should contain the desired modes you want to open the DB
+   in.  This is required.
+
+Returns the result of the last operation in `body`."
+  [[db open-spec] & body]
+  `(let [~db (make-db (get ~open-spec :throws? true))]
+     (open-db! ~db ~open-spec (:modes ~open-spec))
+     (let [res (do ~@body)]
+       (.close ~db)
+       res)))
+
+(defmacro with-dbs
+  "#Very Modestly Managed access to multiple DBs.
+
+Much like `with-db`, but takes multiple DB and open-spec maps for it's
+initial parameters, with simlar syntax to `let`."
+  [dbs-and-specs & body]
+  (assert (not (empty? dbs-and-specs))
+          "dbs-and-specs must not be empty.")
+  (assert (even? (length dbs-and-specs))
+          "dbs-and-specs must be of even length")
+  (if (= 2 dbs-and-specs)
+    `(with-db ~(vec dbs-and-specs) ~@body)
+    `(with-db ~(vec (take 2 dbs-and-specs))
+       (with-dbs ~(drop 2 dbs-and-specs)
+         ~@body))))
+
+;; --------
+;; The following are specially abstracted forms that make using Kyoto Cabinet
 ;; with Clojure's EDN-like data easier.  Alternatives are available if rote binary
-;; needs to be stored.
+;; needs to be stored instead.
 
 (defn- edn-str
   "Takes some Clojure and writes some EDN, safely."
@@ -249,156 +168,3 @@ you need to do something that I haven't thought of."
   (let [cand (pr-str elt)]
     (assert (= elt (edn/read-string cand)))
     cand))
-
-(defn add-edn!
-  "##Add
-
-Adds a key-value pair to the database.  `key` should be a string.
-or a byte array. `value` should be something that can be safely 
-rendered in EDN.
-
-Adding to a database will not overwrite an entry should it already
-exist.
-
-Returns a boolean: true if it succeeded, false if it failed."
-  [key value]
-  (let [edn-value (edn-str value)]
-    (.add db key edn-value)))
-
-(defn add-bin!
-  "##Adding Blobs Of Stuff
-
-Adds a key-value pair to a database.  The `key` should be a string,
-and the `blob` should be a vector or array of bytes.
-
-Returns true on success, false of failure."
-  [key blob]
-  (.add db (.getBytes key) 
-        (if (vector? blob)
-          (to-array blob)
-          blob)))
-
-(defn append-bin!
-  "##Append
-
-Appends a key-value pair to the database.  `key` should be a string.
-`blob` should be a a vector or array of bytes.
-
-Appending will either create a new entry to the DB if the key doesn't
-exist, or it appends `value`'s bytes to the end of the existent entry.
-
-Returns true on success, false on failure."
-  [key blob]
-  (.append db (.getBytes key)
-           (if (vector? blob)
-             (to-array blob)
-             blob)))
-
-(defn get-edn
-  "##Get a record.
-
-Give a key, get crunchy EDN back, or nil if it's not there."
-  [key]
-  (when-let [result (.get key)]
-    (edn/read-string result)))
-
-(defn get-bin
-  "##More Get A Record
-
-Give a key (must be a string), get a byte array back, or nil if it's not there."
-  [key]
-  (when-let [result (.get (.getBytes key))]
-    result))
-
-(defn get-bulk-bin
-  "##Bulky Bins
-
-Same thing as `get-bulk-edn`, but returns a map of key to byte array
-instead."
-  ([keys] (get-bulk keys true))
-  ([keys atomic?]
-     (let [keys (-> (map #(.getBytes %) keys)
-                    to-array-2d)]
-       (when-let [result (.get_bulk db keys)]
-         (->> (partition 2 (seq result))
-              (map (fn [[k v]]
-                     [(String. k) v]))
-              (reduce #(apply append %1 %2) {})))))))
-
-(defn get-bulk-edn
-  "##Get a bunch of records.
-
-Give a collection of keys, get a key-value map of what you asked for, or
-nil if something failed.  You can also specify if this will be an atomic
-operation or not, the default is true if not specified."
-  ([keys] (get-bulk keys true))
-  ([keys atomic?]
-     (-> (get-bulk-bin keys atomic?)
-         (map (fn [[k v]] [k (edn/read-string (String. v))]))
-         (reduce #(apply assoc %1 %2) {}))))
-
-(defn remove-bulk!
-  "##Drop A Bunch Of Blob Or A Block Of EDN
-
-Pass in a vector of keys, and watch their entries disappear! 
-If `atomic?` is false, then the operation is not atomic at all;
-it defaults to true."
-  ([keys] (remove-bulk! keys true))
-  ([keys atomic?]
-     (let [keys (-> (map #(.getBytes %) keys)
-                    to-array-2d)]
-       (.remove_bulk db keys atomic?))))
-
-(defn remove!
-  "Simply deletes a record from the DB.
-
-The provided key must be a string."
-  [key]
-  (.remove db (.getBytes key)))
-
-(defn replace-bin!
-  "Replaces a blob with another one, by key"
-  [key blob]
-  (.replace db (.getBytes key) blob))
-
-(defn replace-edn!
-  "Replaces one EDN entry with another, by key"
-  [key value]
-  (replace-bin! key (-> (edn-str value)
-                        #(.getBytes %))))
-
-(defn sieze-bin!
-  "Grab a record by key, removing it when fetched.  Returns the record."
-  [key]
-  (.sieze db (.getBytes key)))
-
-(defn sieze-edn!
-  "Same thing as `sieze-bin!`, but returns EDN instead."
-  [key]
-  (-> (sieze-bin! key)
-      #(String. %)
-      edn/read-string))
-
-(defn set-bulk-bin!
-  ([kv-map] (set-bulk-bin! kv-map true))
-  ([kv-map atomic?]
-     (let [values (->> (keys kv-map)
-                       (reduce #(conj %1 (.getBytes %2) 
-                                      (get kv-map %2)) [])
-                       (to-array-2d))]
-       (.set_bulk db values atomic?))))
-
-(defn set-bulk-edn!
-  ([kv-map] (set-bulk-edn! kv-map true))
-  ([kv-map atomic?]
-     (set-bulk-bin! 
-      (-> (map (fn [[k v]] 
-                 [k 
-                  (-> (edn-str v)
-                      #(.getBytes %))])
-               kv-map)
-          (reduce #(apply assoc %1 %2) {}))
-      atomic?)))
-
-(defn set-bin!
-  ())
